fad12faa8797f6db1b105572429f57a6121ec454
Lenivaya <xocada@gmail.com>
2019-08-16 13:09:56 +0300

    Add main.go

---
+++ main.go
@@ -0,0 +1,57 @@
+// Package main provides
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Config struct {
+	TelegramBotToken string
+}
+
+func main() {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("Using %s\n", configuration.TelegramBotToken)
+
+	bot, err := tgbotapi.NewBotAPI(configuration.TelegramBotToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		var reply string
+		if update.Message == nil {
+			continue
+		}
+		switch update.Message.Command() {
+		case "help":
+			reply = "This is help"
+		case "@ethereality_bot":
+			reply = "Hello"
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		msg.ReplyToMessageID = update.Message.MessageID
+		bot.Send(msg)
+	}
+}
